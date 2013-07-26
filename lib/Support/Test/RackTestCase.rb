8c02a30490b24e1650f410e503faa6384dff4c20
Eric Bollens <ebollens@ucla.edu>
2013-07-26 11:34:32 -0700

    Fix #7 - Add test foundation and test:app:routes

---
+++ lib/Support/Test/RackTestCase.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'extensions/kernel' if defined?(require_relative).nil?
+require 'rack/test'
+require 'hpricot'
+require_relative 'TestCase'
+
+APP = Rack::Builder.parse_file('config.ru').first
+
+module WebBlocks
+  module BuildServer
+    module Support
+      module Test
+        class RackTestCase < TestCase
+  
+          include Rack::Test::Methods
+
+          def app
+            APP
+          end
+  
+          def assert_last_response_body_html_has_link_with_path url, msg = ''
+            doc = Hpricot(last_response.body)
+            assert (doc/"a").select { |a|  URI.parse(a.attributes['href']).path == url }.length > 0, msg
+          end
+          
+        end
+      end
+    end
+  end
+end
---
+++ lib/Support/Test/TestCase.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+module WebBlocks
+  module BuildServer
+    module Support
+      module Test
+        class TestCase < ::Test::Unit::TestCase
+          
+        end
+      end
+    end
+  end
+end
